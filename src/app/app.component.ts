11e2b8a30414dd2ba25cdc695dd45fa04d723794
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2019-09-08 11:01:00 -0300

    Estudo terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 
+import { Article } from './article/article.model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-reddit';
+  articles: Array<Article>;
+  constructor() {
+    this.articles = [
+      new Article('Angular', 'http://angular.io', 3),
+      new Article('Fullstack', 'http://fullstack.io', 2),
+      new Article('Angular Homepage', 'http://angular.io', 1)
+    ];
+  }
+  addArticle(title: HTMLInputElement, link: HTMLInputElement) {
+    console.log(title.value, link.value);
+    this.articles.push(new Article(title.value, link.value, 0));
+    title.value = '';
+    link.value = '';
+    return false;
+  }
+  sortedArticles(): Array<Article> {
+    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
+  }
 }
